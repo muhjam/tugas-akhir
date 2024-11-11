cf2b07e9fb9533f882e11781e67674b6805d2373
Muhamad Jamaludin <muhhjam@gmail.com>
2024-11-11 12:10:41 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 // pages/_app.js
 
 import './styles/globals.css';
-import '@uiw/react-md-editor/markdown-editor.css';
-// import '@uiw/react-markdown-preview/dist/markdown.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
