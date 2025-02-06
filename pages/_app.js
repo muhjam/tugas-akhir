e4924ad1f779b998623a43f86f44101e84551033
muhjam <muhhjam@gmail.com>
2025-02-06 16:32:41 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-    <title>OpenAI Quickstart</title>
-    <link rel="icon" href="/quest.png" />
+    <title>Tugas Akhir | Jamjam</title>
+    <link rel="icon" href="/math.png" />
   </Head>
   <Component {...pageProps} />
   </>
