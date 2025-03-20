a2510bb528f2c97167c97d80feaff51cc8f2880a
muhjam <muhhjam@gmail.com>
2025-03-20 19:43:04 +0700

    update

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -96,6 +96,12 @@ function generatePrompt ( data )
       \end{array}
       $$"
       "$$\text{Volume} = \text{panjang} \times \text{lebar} \times \text{tinggi} $$"
+      "$$\n
+      A = \begin{bmatrix} 
+      a_{11} & a_{12} & a_{13} \\
+      a_{21} & a_{22} & a_{23} \\
+      \end{bmatrix}\n
+      $$"
 
 
       ---
@@ -205,6 +211,12 @@ function generatePrompt ( data )
       \end{array}
       $$"
       "$$\text{Volume} = \text{panjang} \times \text{lebar} \times \text{tinggi} $$"
+      "$$\n
+      A = \begin{bmatrix} 
+      a_{11} & a_{12} & a_{13} \\
+      a_{21} & a_{22} & a_{23} \\
+      \end{bmatrix}\n
+      $$"
     
     ---
     Warnings:
