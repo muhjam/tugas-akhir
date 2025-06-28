051ef099a10fd1572bf03c8c2772c2c2f30e79d7
muhjam <muhhjam@gmail.com>
2025-06-28 22:36:34 +0700

    update

--- components/modal-prompt/index.js
+++ components/modal-prompt/index.js
@@ -183,6 +183,18 @@ const handleClose = () => {
   onClose();
 };
 
+useEffect(() => {
+  if (isOpen) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'unset';
+  }
+  
+  return () => {
+    document.body.style.overflow = 'unset';
+  };
+}, [isOpen]);
+
 
   if (!isOpen) return null;
 
